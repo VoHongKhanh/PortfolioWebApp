81f1d150d9b020f30afe5b569bbacffb19f6897a
Võ Hồng Khanh <khanhvohong@gmail.com>
2023-11-29 22:06:48 +0700

    Add images, columns and description on the bottom

---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns([1, 3])
+
+with col1:
+    st.image("images/photo.png", width=400)
+
+with col2:
+    st.title("Võ Hồng Khanh")
+
+    content = """
+    Hi, I am Võ Hồng Khanh! I am a Python programmer, teacher, and founder of PythonHow. I graduated in 2017 with a 
+    Master of Information System from Can Tho University in Vietnam. 
+    I focus on using Python for remote sensing, I have worked with companies from various countries, such as the Center 
+    for Conservation Geography, to map and understand Australian ecosystems, image processing with the Swiss In-Terra, 
+    and performing data mining to gain business insights with the Australian Rapid Intelligence. 
+    """
+
+    st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python, Fell free to contact me!
+"""
+
+st.write(content2)
